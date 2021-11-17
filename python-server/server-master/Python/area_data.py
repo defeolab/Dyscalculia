f5fc83bd011068d24ed999707a87bfc63b7fe35d
Ivana Orefice <88187595+ivanaorefice@users.noreply.github.com>
2021-11-17 12:23:33 +0100

    deleting everything

--- python-server/server-master/Python/area_data.py
+++
@@ -1,7 +0,0 @@
-class AreaData:
-
-    def __init__(self, circle_radius, size_of_chicken, average_space_between, number_of_chickens):
-        self.circleRadius = circle_radius
-        self.sizeOfChicken = size_of_chicken
-        self.averageSpaceBetween = average_space_between
-        self.numberOfChickens = number_of_chickens
