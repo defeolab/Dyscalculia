941d112119d39afdd31e9aee7e69b36d44a1a58e
Andrés Abundis <andresabundis@gmail.com>
2022-07-08 13:17:05 +0200

    Adds explanatory comments to the code.

--- filtering_proof.py
+++ filtering_proof.py
@@ -3,35 +3,40 @@ from cv2 import pointPolygonTest
 import numpy as np
 
 def diff_coef(x, y):
+    # Gets K which stands for the angle between the coordinate
+    #       and the X axis.
     k = math.atan(y/x)
-    # print(k)
+    # Calculates the limits that define the cuadrants 
     limit = (math.pi)/4
-    # print(limit)
+    # Get the difficulty coefficient depending on the cuadrant.
     if (x > 0 and y >= 0) or (x < 0 and y <= 0):
         if k >= limit:
-            # print(1.1)
             return k - limit
         else:
-            # print(1.2)
             return limit - k
     elif (x < 0 and y > 0) or (x > 0 and y < 0):
         if abs(k) >= limit:
-            # print(2.1)
             return k + limit
         else:
-            # print(2.2)
             return -limit - k
 
+# Generate the angles that will define the circle points as test coordinates for the 
+#       difficulty coefficiente calculation method.
 circle_points = np.arange(0,2*math.pi,2*math.pi/16)
-# print(circle_points)
+# Input the radio of the test circle to calculate the test coordinates.
 radius = float(input("Input radio of circle: "))
 
 for angle in circle_points:
+    # Generate coordinates
     x, y = radius*math.cos(angle), radius*math.sin(angle)
+    # Handle the coordinates that are so close to the axis, that they should be considered
+    #       as part of each respective axis.
     if x < 0.001 and x > -0.001:
         x = 0
     if y < 0.001 and y > -0.001:
         y = 0
+    # There is no way the numerical variables ratio can be 0 because that would mean that
+    #       there is no animals in the corrals. These combinations are deemed not possible.
     if x == 0.0: 
         print("angle: {}\t x: {}\t\t / y: {}\t / COMBINATION NOT POSSIBLE ".format(round(angle,2), round(x,3), round(y,3)))
     # print("x: {}\t - y: {}\t - coefficient: {}".format(x, y, diff_coef(x, y)))
