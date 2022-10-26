1c967583d060fc90775bb0db6a844b9f5f8b1087
f-blan <c.blangio@gmail.com>
2022-10-26 16:02:33 +0200

    fix: minor fix

--- python-server/server-master/Python/AI/PlayerSimulator.py
+++ python-server/server-master/Python/AI/PlayerSimulator.py
@@ -10,6 +10,8 @@ class PlayerSimulator:
 
 
         self.boundary_vector = unit_vector(np.array([-math.sin(math.radians(alpha)), math.cos(math.radians(alpha))]))
+
+        #basis in the decision boundary space
         self.transform_mat =np.linalg.inv(np.array([[self.boundary_vector[0], self.boundary_vector[1]], [self.boundary_vector[1], -self.boundary_vector[0]]]))
         
 
@@ -19,19 +21,20 @@ class PlayerSimulator:
         #print(trial)
         nd_coord = trial[6]
         nnd_coord = trial[7]
-        
+
+        is_right = nd_coord > 0
         #sharpening effect: add random noise
         nd_coord += np.random.normal(scale=self.sigma)
         nnd_coord += np.random.normal(scale=self.sigma)
 
         trial_vec = unit_vector(np.array([nd_coord, nnd_coord]))
 
-        is_right = nd_coord > 0
-
+        
+        #transform vector in the decision space
         trial_vec = np.dot(self.transform_mat, vcol(trial_vec))
         looks_right = trial_vec > 0
 
-        #assert True == False    
+        #print(f"nd: {nd_coord} - nnd: {nnd_coord} - tv: {trial_vec} - b: {self.transform_mat}")
 
         return bool(looks_right[1] == is_right), 2.0
 
