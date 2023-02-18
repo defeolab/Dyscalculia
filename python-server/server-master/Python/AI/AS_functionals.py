fix: revised NND computation and recomputed the Lookup Tables

python-server/server-master/Python/AI/AS_functionals.py
@@ -15,7 +15,7 @@ from AI.ai_consts import *
 DEBUG_D = False
 DEBUG_S = False
 DEBUG_PC = False
-PATH_FOR_C_ABLATION = ".\\AI\\precomputed_data\\PDEP\\C_ablation5"
+PATH_FOR_C_ABLATION = ".\\AI\\precomputed_data\\PDEP\\C_ablation"
 PATH_FOR_N_ABLATION = ".\\AI\\precomputed_data\\PDEP\\N_trials_ablation_fixed"
 PERFORM_ABLATION_C = False
 PERFORM_ABLATION_N_TRIALS = False
@@ -38,11 +38,11 @@ if PERFORM_ABLATION_C == False:
     filepath = os.path.join(PATH_FOR_C_ABLATION, "Cs.npy")
     CS : np.ndarray = np.load(filepath)
 
-    filepath = os.path.join(PATH_FOR_C_ABLATION, "alpha_errors_by_Cs.npy")
+    filepath = os.path.join(PATH_FOR_C_ABLATION, "errors_by_Cs.npy")
     ERR_A_CS : np.ndarray = np.load(filepath)
 
     filepath = os.path.join(PATH_FOR_C_ABLATION, "sigma_errors_by_Cs.npy")
-    ERR_S_CS : np.ndarray = np.load(filepath)
+    #ERR_S_CS : np.ndarray = np.load(filepath)
 
 if PERFORM_ABLATION_N_TRIALS == False:
     #filepath = os.path.join(PATH_FOR_N_ABLATION, "best_n_trials_index.npy")

python-server/server-master/Python/AI/ai_plot.py
@@ -106,7 +106,8 @@ def plot_trials(boundary_vector: np.ndarray,
                 estimated_std: np.ndarray = None,
                 plot_fs: bool = False,
                 title: str = None,
-                plot_norm: bool = False):
+                plot_norm: bool = False,
+                plot_links: bool = False):
 
     if figsaver is not None:
         #just a speedup, avoid plotting if you're not going to show or save the figure
@@ -131,6 +132,10 @@ def plot_trials(boundary_vector: np.ndarray,
 
     for i, coord in enumerate(coords):
         #print(corrects[i])
+        if i % 2 == 1 and plot_links:
+            prev = coords[i-1]
+            ax.plot([prev[0], coord[0]], [prev[1], coord[1]], color="blue")
+
         ax.scatter(coord[0], 
             coord[1], 
             color = colors[corrects[i]])
@@ -257,6 +262,8 @@ def plot_stats( statlist: List[List[float]],
     to_save = []
     to_save.append((x, f"x_data_{main_stat}"))
     for i, dl in enumerate(statlist):
+        if len(dl) ==0:
+            continue
         ax.plot(x, dl, color= colors[i], label=labels[i])
         to_save.append((dl, labels[i]))
     
@@ -286,7 +293,12 @@ def plot_stats( statlist: List[List[float]],
     if figsaver is None:
         plt.show()
     else:
-        figsaver.save_summary_stats(f"{labels[0]}-{labels[1]}")
+        name = labels[0]
+        for i, l in enumerate(labels):
+            if i ==0:
+                continue
+            name += f"-{l}"
+        figsaver.save_summary_stats(name)
         if save_as_ndarray:
             figsaver.save_np_array(to_save, "main")
     
@@ -314,6 +326,8 @@ def plot_monthly_stats( statlist: List[List[float]],
         x = np.linspace(1, length, length)
 
     for l in statlist:
+        if len(l) ==0:
+            continue
         daily_stat = vec_reshape_by_day(l, tpd)
         reduced = []
         for ds in daily_stat:
@@ -324,6 +338,8 @@ def plot_monthly_stats( statlist: List[List[float]],
     to_save = []
     to_save.append((x, "x_data"))
     for i, dl in enumerate(daily_statlist):
+        if len(dl) ==0:
+            continue
         ax.plot(x, dl, color= colors[i], label=labels[i])
         to_save.append((dl, f"{month_n}_{labels[i]}"))
         
@@ -398,9 +414,9 @@ def make_tables( main_stat: List[float],
             avg_dist = np.average(dist)
             dist_std = np.std(dist)
             max_dist = np.max(dist)
-            monthly_data.append(avg_dist)
-            monthly_data.append(dist_std)
-            monthly_data.append(max_dist)
+            monthly_data.append(round(avg_dist,2))
+            monthly_data.append(round(dist_std,2))
+            monthly_data.append(round(max_dist,2))
         
         data.append(monthly_data)
 
@@ -413,9 +429,9 @@ def make_tables( main_stat: List[float],
         avg_dist = np.average(dist)
         dist_std = np.std(dist)
         max_dist = np.max(dist)
-        last_row.append(avg_dist)
-        last_row.append(dist_std)
-        last_row.append(max_dist)
+        last_row.append(round(avg_dist,2))
+        last_row.append(round(dist_std,2))
+        last_row.append(round(max_dist,2))
 
     data.append(last_row)
     

python-server/server-master/Python/simulate_main.py
@@ -275,17 +275,18 @@ if __name__ == "__main__":
         [0.2],
         [0.1],
         [0.3],
-        [MAX_SIGMA-0.1]
+        [MAX_SIGMA-0.1],
+        [0.10, 0.4]
         ]
-    alpha_i = 7
-    sigma_i = 2
+    alpha_i = 8
+    sigma_i = 5
 
 
     probs = [0.10, 0.30, 0.80]
     diffs = [(0.1,0.1), (0.4, 0.4), (0.8, 0.8), (0.95,0.95)]
     modes = ["filtering", "sharpening"]
 
-    days =180
+    days =60
     trials_per_day = 30
     interval = 15
 
@@ -309,7 +310,7 @@ if __name__ == "__main__":
     target_n_trials = np.linspace(100, 1800, 18)
     target_slopes = -np.logspace(-4, -2, 10, base=10)/trials_per_day
     
-    local_target_slope = target_slopes[7]
+    local_target_slope = target_slopes[9]
     update_child = True
     improver_parameters_options =   [
                                         [0.45, 0.003, 1],
@@ -332,7 +333,7 @@ if __name__ == "__main__":
     make_plots = True
     save_ablation = False
     n_runs = 2
-    suite_name = "test_clean_iterations_impr"
+    suite_name = "high_slope_regular"
     difficulties = ["regular", "easy"]
     diff_i = 1
 
@@ -344,7 +345,7 @@ if __name__ == "__main__":
 
     start_time = time.time()
 
-    sr.simulation_suite()
+    #sr.simulation_suite()
     #sr.ablation_C(last_n_days, n_runs)
     #sr.ablation_n_trials(target_slopes, target_n_trials, n_runs)
     discarded_slopes_i = [0,1,2,3,4,5]
@@ -359,13 +360,13 @@ if __name__ == "__main__":
             print(f">>>>> slope is {local_target_slope}")
         
 
-        suite_name = f"unified_plots_easy_v2_{i}"
+        suite_name = f"unified_plots_easy_v3_{i}"
         sr = SimulationsRunner( days, trials_per_day, interval, evaluator, kids_ds, update_evaluator_stats, update_child, suite_name, 
                             target_prob, target_diff, mode, save_trials, save_plots, alphas[alpha_i], sigmas[sigma_i], mock, estimate_step,
                             target_C, make_plots, save_ablation, estimation_duration, 
                             estimator_type, init_evaluator_stats, estimator_max_trials, estimator_min_trials,improver_type, 
                             [local_target_slope*MAX_ALPHA, local_target_slope*MAX_SIGMA, 1], difficulties[diff_i])
-        #sr.simulation_suite()
+        sr.simulation_suite()
 
 
     duration = 1000  # milliseconds

python-server/server-master/Python/tests.py
@@ -142,7 +142,7 @@ class TestAI(unittest.TestCase):
                 anns.append("")
 
         vec = np.array([0,1])
-        plot_trials(vec, raw, corrects, anns, ann_str=True, norm_lim=True)
+        plot_trials(vec, raw, corrects, anns, ann_str=True, norm_lim=True, plot_links = True)
 
     def test_3D_plot(self):
         player = PlayerSimulator(45, 0.5)
@@ -342,7 +342,7 @@ class TestAI(unittest.TestCase):
         print(BEST_CS)
         #rint(CS[BEST_CS[1]])
         #print(find_expected_optimal_C(np.array([-0.6,0.4]), 0.50))
-        print(CONFIGS)
+        #print(CONFIGS)
         #print(CS)
         #print(ERR_CS)
 
@@ -512,18 +512,21 @@ class TestAI(unittest.TestCase):
         #nv = np.array([17,17,17, 13, 9, 9, 6, 4, 1, 0])
         #np.save(filepath, nv)
 
-        x = SLOPE_CONFIGS[0]
+        x = np.abs(SLOPE_CONFIGS[0])
         y = N_TRIALS[BEST_N_INDEXES]
 
-
+        valid_indexes = np.array([0,4,5,6,7,8,9])
+        x = x[valid_indexes]
+        y = y[valid_indexes]
         #fig = plt.figure()
         #ax = fig.gca()
-        #ax.set_xscale("log")
+        
         plt.scatter(x, y, color = "red")
         plt.plot(x, y, color = "blue")
-        plt.xlabel("slopes (unit/trial)")
-        plt.ylabel("window width (number of trials)")
-
+        plt.xlabel("normalized unsigned slopes (unit/trial)")
+        plt.ylabel("Optimal z")
+        plt.xscale("log")
+        plt.grid(True)
         plt.show()
     
     def test_trial_mirroring(self):
@@ -578,7 +581,24 @@ class TestAI(unittest.TestCase):
         target_slopes = target_slopes[6:]
         labels = [f"Daily Slope = {round(t, 2)}°" for t in target_slopes*MAX_ALPHA]
         labels.append("Null slope")
-        plot_comparisons(root_path, labels, monthly=True, suffix_set=[str(i) for i in range(6,10+1)], title="Second Pass alpha", xlabel="day", plot_dists=True)
+        folder_prefix = "unified_plots_easy_v3_"
+        subfolder_name="alpha_55_sigma_30"
+
+        plot_comparisons(root_path, labels, monthly=True, folder_prefix=folder_prefix, suffix_set=[str(i) for i in range(6,10+1)], title="Second Pass alpha", xlabel="day", plot_dists=True, subfolder_name=subfolder_name)
+
+        labels = [f"Daily Slope = {round(t, 4)} units" for t in target_slopes*MAX_SIGMA]
+        labels.append("Null slope")
+        plot_comparisons(root_path, labels, monthly=True, suffix_set=[str(i) for i in range(6,10+1)], title="Second Pass Sigma", xlabel="day", metric_name="second_pass_sigma", main_stat="sigma", subfolder_name=subfolder_name, plot_dists=True,folder_prefix=folder_prefix)
+
+        labels = [f"Daily Unified Slope = {round(t, 4)} units" for t in target_slopes]
+        labels.append("Null slope")
+        plot_comparisons(root_path, labels, monthly=False, suffix_set=[str(i) for i in range(6,10+1)], title="Accuracy (easy mode)", xlabel="day", metric_name="cumulative_accuracy", main_stat="accuracy", subfolder_name=subfolder_name, xlength=60,folder_prefix=folder_prefix)
+
+        labels = [f"Daily Unified Slope = {round(t, 4)} units" for t in target_slopes]
+        labels.append("Null slope")
+        plot_comparisons(root_path, labels, monthly=False, suffix_set=[str(i) for i in range(6,10+1)], title="Accuracy (regular mode)", xlabel="day", metric_name="cumulative_accuracy", main_stat="accuracy", subfolder_name=subfolder_name, xlength=60,folder_prefix=folder_prefix)
+        """
+        plot_comparisons(root_path, labels, monthly=True, folder_prefix=folder_prefix, suffix_set=[str(i) for i in range(6,10+1)], title="Second Pass alpha", xlabel="day", plot_dists=True)
 
         labels = [f"Daily Slope = {round(t, 4)} units" for t in target_slopes*MAX_SIGMA]
         labels.append("Null slope")
@@ -591,7 +611,7 @@ class TestAI(unittest.TestCase):
         labels = [f"Daily Unified Slope = {round(t, 4)} units" for t in target_slopes]
         labels.append("Null slope")
         plot_comparisons(root_path, labels, monthly=False, suffix_set=[str(i) for i in range(6,10+1)], title="Accuracy (regular mode)", xlabel="day", metric_name="cumulative_accuracty", main_stat="accuracy", subfolder_name="alpha_80_sigma_40", xlength=65)
-
+        """
     def test_unified_plots(self):
         root_path = os.path.join(BASE_PATH_FOR_PICS, "PDEP")
         n = 8
@@ -663,7 +683,7 @@ class TestAI(unittest.TestCase):
         corrects = [True, False, False, True, True, True]
         anns = ["" for t in trials]
         std = 0.2
-        #plot_trials([-1, 2], trials, corrects, anns, ann_str=True, sharp_std=std)
+        plot_trials([-1, 2], trials, corrects, anns, ann_str=True, sharp_std=std)
         #plot_gaussian_3D([0.2, 0.3], 0.3)
         #plot_trials([-1,1], [to_mock_trial(-0.1,-0.1)], [True], [""],ann_str=True, sharp_std=0.3, plot_fs=True, plot_norm=True)
         
@@ -681,7 +701,7 @@ class TestAI(unittest.TestCase):
 
         for i,t in enumerate(trials):
             a = i
-            plot_1d_gaussians(t, unit_vector([-1,1]), std, i+1) 
+            #plot_1d_gaussians(t, unit_vector([-1,1]), std, i+1) 
         bv=unit_vector([-1,1])
         transform_mat = np.linalg.inv(np.array([[bv[0], bv[1]], [bv[1], -bv[0]]]))
         max_dec_score = 2*math.sqrt(2)
@@ -698,6 +718,66 @@ class TestAI(unittest.TestCase):
         #plot_trials(unit_vector([-1, 1]), mt, corrects, anns, ann_str=True, plot_fs=True,sharp_std=std, title=f"Target error probability: {target_error_prob}")
 
 
+    def test_plot_generic_comparison(self):
+        root_path = os.path.join(BASE_PATH_FOR_PICS, "PDEP")
+
+        root_path_1 = os.path.join(root_path, f"unified_plots_regular_v2_10", "alpha_55_sigma_30")
+    
+        f_a_alpha_1 = os.path.join(root_path_1, "actual_alpha.npy")
+        a_alpha_1 = np.load(f_a_alpha_1)
+
+        root_path_2 = os.path.join(root_path, f"unified_plots_regular_v2_7", "alpha_55_sigma_30")
+    
+        f_a_alpha_2 = os.path.join(root_path_2, "actual_alpha.npy")
+        a_alpha_2 = np.load(f_a_alpha_2)
+
+        root_path_3 = os.path.join(root_path, f"unified_plots_regular_v2_9", "alpha_55_sigma_30")
+    
+        f_a_alpha_3 = os.path.join(root_path_3, "actual_alpha.npy")
+        a_alpha_3 = np.load(f_a_alpha_3)
+
+        target_slopes = -np.logspace(-4, -2, 10, base=10)
+        
+        trials_per_day = 30
+
+        l = 1500
+        plot_stats  (   [   
+                            a_alpha_1[0:l] + 20,
+                            a_alpha_2[0:l] + 20,
+                            a_alpha_3[0:l] + 20
+                        ],
+                        l,
+                        [f"lr = {0.0} deg/it", f"lr = {round(target_slopes[7]*MAX_ALPHA, 2)} deg/it", f"lr = {round(target_slopes[9]*MAX_ALPHA, 2)} deg/it"], 
+                        main_stat="Alpha (degrees)",
+                        lim_bounds=[-5, 95],
+                        save_as_ndarray=False,
+                        title= f"Alpha improvement",
+                        xlabel="iterations (it)",
+                    )
+
+    def test_distance(self):
+        root_path = os.path.join(BASE_PATH_FOR_PICS, "PDEP")
+
+        root_path_1 = os.path.join(root_path, f"fp_sp_fixed_studies", "alpha_10_sigma_40")
+
+        f_a_a = os.path.join(root_path_1, "actual_alpha.npy")
+        aa = np.load(f_a_a)
+
+        f_a_a = os.path.join(root_path_1, "estimated_alpha.npy")
+        fp_a = np.load(f_a_a)
+
+        print(np.sum(np.abs(fp_a - aa))/fp_a.shape[0])
+
+        f_a_s = os.path.join(root_path_1, "actual_sigma.npy")
+        a_s = np.load(f_a_s)
+
+        f_a_s = os.path.join(root_path_1, "estimated_sigma.npy")
+        fp_s = np.load(f_a_s)
+
+        print(np.sum(np.abs(fp_s - a_s))/fp_s.shape[0])
+
+
+        
 
 if __name__ == "__main__":
     tc = TestAI()
@@ -708,7 +788,7 @@ if __name__ == "__main__":
     #tc.test_PDEP_Evaluator()
     #tc.test_player_cycle_simple()
     #tc.test_player_cycle_PDEP()
-    #tc.test_trial_adapter()
+    tc.test_trial_adapter()
     #tc.test_3D_plot()
     #tc.test_precompute()
     #tc.test_AS()
@@ -726,7 +806,9 @@ if __name__ == "__main__":
     #tc.test_trial_mirroring()
     #tc.test_joint_plots()
     #tc.test_unified_plots()
-    tc.test_plots_for_thesis()
+    #tc.test_plots_for_thesis()
+    #tc.test_plot_generic_comparison()
+    #tc.test_distance()
 
     duration = 1000  # milliseconds
     freq = 440  # Hz